f34cdf618af399bcdea92f16f5b2e719dbcbda27
Osama <osamadoage0@gmail.com>
2025-12-17 17:24:56 +0300

    disable for now authintication for create user endpoint

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 
+// just for now, allow creating users without restrictions
+Route::post('/users/create', [UserController::class, "createUser"]);
+
+
 // Protected routes (require authentication)
 Route::middleware('auth:sanctum')->group(function () {
     Route::patch("/users/update-my-profile", [UserController::class, "updateProfile"]);
@@ -15,7 +19,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/users/delete-my-profile', [UserController::class, "deleteProfile"]);
     Route::get('/users/list', [UserController::class, "listUsers"]);
     Route::get('/users/{user}', [UserController::class, "getUser"]);
-    Route::post('/users/create', [UserController::class, "createUser"]);
     Route::patch('/users/update/{user}', [UserController::class, "updateUser"]);
     Route::delete('/users/delete/{user}', [UserController::class, "deleteUser"]);
 
